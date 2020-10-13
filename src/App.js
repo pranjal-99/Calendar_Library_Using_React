7c44956579fb6a702061c6caf0f9bcf0dc42df62
Pranjal Gupta <66473164+pranjal-99@users.noreply.github.com>
2020-10-13 15:19:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Calendar from "./Calendar.js";
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Making Calendar Library</h2>
+      <Calendar />
+    </div>
+  );
+}
